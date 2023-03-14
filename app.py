a2627a992c53807507ea02ac6a50a3e564028552
dolsen0 <derek.olsen@gmail.com>
2023-03-14 18:40:54 -0400

    Prints 10 businesses from highest rating to lowest based on your area code or other location params

---
+++ app.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+# Load the API key from the JSON file
+with open('apiKey.json') as api_key_json:
+    api_key = json.load(api_key_json)['api_key']
+
+# Set the API endpoint and parameters
+api_endpoint = 'https://api.yelp.com/v3/businesses/search'
+params = {
+    'location': '33065',
+    'term': 'cuban',
+    'limit': 10,
+    'sort_by': 'rating'
+}
+
+# Set the headers with the API key
+headers = {
+    'Authorization': f'Bearer {api_key}'
+}
+
+# Make the request to the API
+response = requests.get(api_endpoint, headers=headers, params=params)
+
+# Convert the response to a JSON object
+businesses = json.loads(response.text)
+
+# Print the names of the businesses
+for business in businesses['businesses']:
+    print(business['name'])
